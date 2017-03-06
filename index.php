f474022f77c5473dd697697689297fd669dcd0dd
anggasa <anqghasiyaputra@hotmail.com>
2017-03-07 01:22:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,33 +2,34 @@
 <html>
   <head>
     <title>CREAT SSH</title>
- 
- <!-- include css file here-->
+	
+	<!-- include css file here-->
    <link rel="stylesheet" href="https://raw.githubusercontent.com/anggasa/tes/master/style.css"/>
    
- <!-- include JavaScript file here-->
- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
- <script type="text/javascript" src="https://raw.githubusercontent.com/anggasa/tes/master/metode.js"></script>
-  </head> 
+	<!-- include JavaScript file here-->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://raw.githubusercontent.com/anggasa/tes/master/metode.js"></script>
+  </head>	
   <body>
-<img src="https://raw.githubusercontent.com/anggasa/tes/master/ba.png" align="left" height="75px" width="350px">
-<form action="http://188.166.241.202/index.php" method="POST">
- <div class="container">
-  <div class="main">
-   <h2>SSH SGDO-1</h2><hr/>  
-   
-   <label>User :</label>
-   <input type="text" name="user" id="user" required="required"/>
-   
-   <label>Passwarod :</label>
-   <input type="text" name="pass" id="pass" required="required"/>
+<img src="ba.png" align="left" height="75px" width="350px">
+	<div class="container">
+		<div class="main">
 
-            <input type="submit" name="submit" id="submit" value="CREAT">   
-   
+<form action="https://raw.githubusercontent.com/anggasa/tes/master/index.php" method="POST">
 
-  <form action="http://188.166.241.202/proses.php" method="POST">
+			<h2>SSH SGDO-1</h2><hr/>		
+			
+			<label>User :</label>
+			<input type="text" name="user" id="user" required="required"/>
+			
+			<label>Passwarod :</label>
+			<input type="text" name="pass" id="pass" required="required"/>
+
+            <input type="submit" name="submit" id="submit" value="CREAT">	
+		<form action="http://188.166.241.202/proses.php" method="POST">
  </form>
-  </div>
+ </form>
+		</div>
    </div>
   </body>
 </html>
