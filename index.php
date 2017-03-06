d3e013eea1f7991cfb21e2d9eb4bf60ccf7b999f
anggasa <anqghasiyaputra@hotmail.com>
2017-03-07 01:09:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,14 @@
     <title>CREAT SSH</title>
  
  <!-- include css file here-->
-   <link rel="stylesheet" href="css/style.css"/>
+   <link rel="stylesheet" href="https://raw.githubusercontent.com/anggasa/tes/master/style.css"/>
    
  <!-- include JavaScript file here-->
  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
- <script type="text/javascript" src="http://188.166.241.202/js/metode.js"></script>
+ <script type="text/javascript" src="https://raw.githubusercontent.com/anggasa/tes/master/metode.js"></script>
   </head> 
   <body>
- <form action="http://188.166.241.202/index.php" method="POST">
-<img src="http://188.166.241.202/ba.png" align="left" height="75px" width="350px">
+<img src="https://raw.githubusercontent.com/anggasa/tes/master/ba.png" align="left" height="75px" width="350px">
  <div class="container">
   <div class="main">
    <h2>SSH SGDO-1</h2><hr/>  
