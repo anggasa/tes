0033eb76443202eb6904ce269f5b263a3bf60faf
anggasa <anqghasiyaputra@hotmail.com>
2017-03-07 00:24:52 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CREAT SSH</title>
+ 
+ <!-- include css file here-->
+   <link rel="stylesheet" href="css/style.css"/>
+   
+ <!-- include JavaScript file here-->
+ <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+ <script type="text/javascript" src="http://188.166.241.202/js/metode.js"></script>
+  </head> 
+  <body>
+ <form action="http://188.166.241.202/index.php" method="POST">
+<img src="http://188.166.241.202/ba.png" align="left" height="75px" width="350px">
+ <div class="container">
+  <div class="main">
+   <h2>SSH SGDO-1</h2><hr/>  
+   
+   <label>User :</label>
+   <input type="text" name="user" id="user" required="required"/>
+   
+   <label>Passwarod :</label>
+   <input type="text" name="pass" id="pass" required="required"/>
+
+            <input type="submit" name="submit" id="submit" value="CREAT">   
+   
+
+  <form action="http://188.166.241.202/proses.php" method="POST">
+ </form>
+  </div>
+   </div>
+  </body>
+</html>
